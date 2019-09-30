ac1b2da5bf1be1d757f42adb4efd0d4cd1e38a60
Pinto <pintocr@media-saturn.com>
2019-09-30 14:12:40 +0200

    sign up implemented

--- starter-code/routes/user.js
+++ starter-code/routes/user.js
@@ -7,17 +7,59 @@ const User = require("../models/user");
 const bcrypt = require("bcrypt");
 const bcryptSalt = 10;
 
+
+router.get("/sign-Up", (req, res, next) => {
+    res.render("signup");
+  });
+
+  router.post("/sign-Up", (req, res, next) => {
+    const username = req.body.username;
+    const email = req.body.email;
+    const password = req.body.password;
+    if (username === "" || password === "" || email === "") {
+      res.render("signUp", {
+        errorMessage: "Indicate a Username, Email and a Password to sign up"
+      });
+      return;
+    }
+    User.findOne({ "email": email })
+      .then(user => {
+        if (user !== null) {
+          res.render("signUp", {
+            errorMessage: "The Email "+email+" already exists!"
+          });
+          return;
+        }
+        const salt = bcrypt.genSaltSync(bcryptSalt);
+        const hashPass = bcrypt.hashSync(password, salt);
+        User.create({
+          username,
+          email,
+          password: hashPass
+        })
+          .then(() => {
+            res.redirect("/");
+          })
+          .catch(error => {
+            console.log(error);
+          })
+      })
+      .catch(error => {
+        next(error);
+      })
+  });
+
 router.get("/log-In", (req, res, next) => {
     res.render("login");
   });
 
-  router.post("/login", (req, res, next) => {
+  router.post("/log-In", (req, res, next) => {
     const theEmail = req.body.email;
     const thePassword = req.body.password;
   
     if (theEmail === "" || thePassword === "") {
       res.render("login", {
-        errorMessage: "Please enter both, username and password to log-In."
+        errorMessage: "Please enter both, Email and Password to log-In."
       });
       return;
     }
@@ -33,6 +75,8 @@ router.get("/log-In", (req, res, next) => {
         if (bcrypt.compareSync(thePassword, user.password)) {
           // Save the login in the session!
             req.session.currentUser = user;
+            req.app.locals.session = true;
+            session = "hi";
           res.redirect("/");
         } else {
           res.render("login", {
@@ -43,6 +87,15 @@ router.get("/log-In", (req, res, next) => {
     .catch(error => {
       next(error);
     })
+  });
+
+  router.post("/logout", (req, res, next) => {
+    req.session.destroy((err) => {
+      // cannot access session here
+      
+      req.app.locals.session = false;
+      res.redirect("/");
+    });
   });
 
   module.exports = router;
