42dc0a8e069e80765b10ed376160975028a1156f
dennisdemund <demundd@mediamarktsaturn.com>
2019-10-04 13:15:50 +0200

    fixed multiple entries

--- routes/gag.js
+++ routes/gag.js
@@ -3,7 +3,34 @@ const router  = express.Router();
 const Post = require("../models/post");
 
 router.post("/like/:postId", (req, res, next) => {
+  Post.update({_id: req.params.postId}, {$pull:{ downvotes: req.session.currentUser._id}})
+  .then((user) => {
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+
+  Post.update({_id: req.params.postId}, {$pull:{ upvotes: req.session.currentUser._id}})
+  .then((user) => {
+    if(user.nModified == 1) {
+    }
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+
   Post.update({_id: req.params.postId}, {$push:{ upvotes: req.session.currentUser._id}})
+  .then((user) => {
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+
+});
+
+router.post("/dislike/:postId", (req, res, next) => {
+  Post.update({_id: req.params.postId}, {$pull:{ upvotes: req.session.currentUser._id}})
   .then((user) => {
   })
   .catch((error) => {
@@ -15,10 +42,8 @@ router.post("/like/:postId", (req, res, next) => {
   })
   .catch((error) => {
     console.log(error);
-  })
-});
+  });
 
-router.post("/dislike/:postId", (req, res, next) => {
   Post.update({_id: req.params.postId}, {$push:{ downvotes: req.session.currentUser._id}})
   .then((user) => {
   })
@@ -26,12 +51,6 @@ router.post("/dislike/:postId", (req, res, next) => {
     console.log(error);
   });
 
-  Post.update({_id: req.params.postId}, {$pull:{ upvotes: req.session.currentUser._id}})
-  .then((user) => {
-  })
-  .catch((error) => {
-    console.log(error);
-  })
 });
 
 router.get("/gag/:gagId", (req, res, next) => {
